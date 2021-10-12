cf667275b3706379d3ea60a0a5ec2928aab53eb1
sumit2789 <sumit.kumar@sastechstudio.com>
2021-10-12 17:46:16 +0530

    FLM-83 code refactor

--- user/src/main/java/com/sts/finncub/usermanagement/config/RequestFilter.java
+++ user/src/main/java/com/sts/finncub/usermanagement/config/RequestFilter.java
@@ -5,7 +5,6 @@ import com.sts.finncub.core.constants.RestMappingConstants;
 import com.sts.finncub.core.entity.UserSession;
 import com.sts.finncub.core.exception.UnauthorizedException;
 import com.sts.finncub.core.util.TokenValidator;
-import com.sts.finncub.usermanagement.assembler.UserSessionConverter;
 import com.sts.finncub.usermanagement.response.Response;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,65 +25,64 @@ import java.io.IOException;
 @Import(TokenValidator.class)
 public class RequestFilter implements Filter {
 
-	@Autowired
-	TokenValidator tokenValidator;
+    @Autowired
+    TokenValidator tokenValidator;
 
-	@Override
-	public void init(FilterConfig filterConfig) throws ServletException {
-	}
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
 
-	@Override
-	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
-			throws IOException, ServletException {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
 
-		boolean isValidRequest = true;
-		if (servletRequest instanceof HttpServletRequest
-				&& ((HttpServletRequest) servletRequest).getRequestURI().contains("/api")) {
-			HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
-			String token = httpServletRequest.getHeader("Authorization");
-			log.info("Headers - {}",httpServletRequest.getHeaderNames());
-			if (token != null && !token.isEmpty()) {
-				try {
-					String tokenValue = token.split(" ")[1];
-					Object userSessionObject = tokenValidator.validateToken(tokenValue);
-					UserSession userSession = UserSessionConverter.convert(userSessionObject);
-					if(userSession.getUserId() != null) {
-						setSecurityContext(userSession);
-					}else throw new UnauthorizedException(RestMappingConstants.AUTHENTICATION_FAILED);
-				} catch (Exception exception) {
-					log.warn("Request is not valid - exception {}" + exception.getMessage());
-					isValidRequest = false;
-				}
-			} else {
-				log.info("Token not found - value- {}",token);
-				log.warn("Request is not valid - ");
-				isValidRequest = false;
-			}
-		}
-		if (isValidRequest) {
-			log.info("Request is valid");
-			filterChain.doFilter(servletRequest, servletResponse);
-		} else {
-			byte[] response = restResponseBytes(new Response<>(RestMappingConstants.AUTHENTICATION_FAILED, HttpStatus.UNAUTHORIZED));
-			servletResponse.getOutputStream().write(response);
-			((HttpServletResponse) servletResponse).setHeader("Content-Type", "application/json");
-			((HttpServletResponse) servletResponse).setStatus(401);
-		}
-	}
+        boolean isValidRequest = true;
+        if (servletRequest instanceof HttpServletRequest
+                && ((HttpServletRequest) servletRequest).getRequestURI().contains("/api")) {
+            HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+            String token = httpServletRequest.getHeader("Authorization");
+            log.info("Headers - {}", httpServletRequest.getHeaderNames());
+            if (token != null && !token.isEmpty()) {
+                try {
+                    String tokenValue = token.split(" ")[1];
+                    UserSession userSession = tokenValidator.validateTokenAndReturnUserSession(tokenValue);
+                    if (userSession.getUserId() != null) {
+                        setSecurityContext(userSession);
+                    } else throw new UnauthorizedException(RestMappingConstants.AUTHENTICATION_FAILED);
+                } catch (Exception exception) {
+                    log.warn("Request is not valid - exception {}" + exception.getMessage());
+                    isValidRequest = false;
+                }
+            } else {
+                log.info("Token not found - value- {}", token);
+                log.warn("Request is not valid - ");
+                isValidRequest = false;
+            }
+        }
+        if (isValidRequest) {
+            log.info("Request is valid");
+            filterChain.doFilter(servletRequest, servletResponse);
+        } else {
+            byte[] response = restResponseBytes(new Response<>(RestMappingConstants.AUTHENTICATION_FAILED, HttpStatus.UNAUTHORIZED));
+            servletResponse.getOutputStream().write(response);
+            ((HttpServletResponse) servletResponse).setHeader("Content-Type", "application/json");
+            ((HttpServletResponse) servletResponse).setStatus(401);
+        }
+    }
 
-	private void setSecurityContext(UserSession userSession) {
-		Authentication authentication = new UsernamePasswordAuthenticationToken(userSession, null, null);
-		SecurityContextHolder.getContext().setAuthentication(authentication);
-	}
+    private void setSecurityContext(UserSession userSession) {
+        Authentication authentication = new UsernamePasswordAuthenticationToken(userSession, null, null);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
 
-	@Override
-	public void destroy() {
+    @Override
+    public void destroy() {
 
-	}
+    }
 
-	private byte[] restResponseBytes(Response response) throws IOException {
-		String serialized = new ObjectMapper().writeValueAsString(response);
-		return serialized.getBytes();
-	}
+    private byte[] restResponseBytes(Response response) throws IOException {
+        String serialized = new ObjectMapper().writeValueAsString(response);
+        return serialized.getBytes();
+    }
 
 }
