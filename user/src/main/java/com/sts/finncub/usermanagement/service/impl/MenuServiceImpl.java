4795cc29e45508211e7379d44207a76325db1b1c
Saloni Bansal <saloni.bansal@sastechstudio.com>
2021-12-30 17:01:44 +0530

    Menu api removed

--- user/src/main/java/com/sts/finncub/usermanagement/service/impl/MenuServiceImpl.java
+++ user/src/main/java/com/sts/finncub/usermanagement/service/impl/MenuServiceImpl.java
@@ -1,25 +1,18 @@
 package com.sts.finncub.usermanagement.service.impl;
 
-import com.sts.finncub.core.dto.MenuMasterDto;
-import com.sts.finncub.core.entity.Menu;
 import com.sts.finncub.core.entity.MenuView;
 import com.sts.finncub.core.entity.UserSession;
 import com.sts.finncub.core.exception.ObjectNotFoundException;
 import com.sts.finncub.core.repository.MenuRepository;
-import com.sts.finncub.core.repository.MenuRoleMappingRepository;
-import com.sts.finncub.core.repository.RoleMasterRepository;
 import com.sts.finncub.core.service.UserCredentialService;
 import com.sts.finncub.usermanagement.assembler.MenuResponseConverter;
 import com.sts.finncub.usermanagement.response.MenuResponse;
-import com.sts.finncub.usermanagement.response.Response;
 import com.sts.finncub.usermanagement.service.MenuService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -28,16 +21,11 @@ public class MenuServiceImpl implements MenuService {
 
     private final UserCredentialService userCredentialService;
     private final MenuRepository menuRepository;
-    private final MenuRoleMappingRepository menuRoleMappingRepository;
-    private final RoleMasterRepository roleMasterRepository;
 
     @Autowired
-    MenuServiceImpl(UserCredentialService userCredentialService, MenuRepository menuRepository,
-                    MenuRoleMappingRepository menuRoleMappingRepository, RoleMasterRepository roleMasterRepository) {
+    MenuServiceImpl(UserCredentialService userCredentialService, MenuRepository menuRepository) {
         this.userCredentialService = userCredentialService;
         this.menuRepository = menuRepository;
-        this.menuRoleMappingRepository = menuRoleMappingRepository;
-        this.roleMasterRepository = roleMasterRepository;
     }
 
     @Override
