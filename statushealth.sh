29572c5be43d1e97b451d2d74bcaedf0d97bdfca
Devops Admin <devops@sastechstudio.com>
2022-03-15 19:29:24 +0000

    statushealth.sh edited online with Bitbucket

--- statushealth.sh
+++ statushealth.sh
@@ -8,12 +8,13 @@
           status=$(aws ecs describe-services --cluster $2 --services $1 --region ap-south-1 && grep message && head -n 1 && grep steady)
     # Wait to see if more than 1 deployment stays running
     # If the wait time has passed, we need to roll back
-    if ( ${status} -eq 0);then
+    if ( ${status} -eq 0); then
       echo "ecs service $1 running in $2"
       DEPLOYMENT_SUCCESS="true"
       # Exit the loop.
       i=$TIMEOUT
     else
+      echo "Waiting for service $1 to be steady"
       sleep $every
       i=$(( $i + $every ))
     fi
