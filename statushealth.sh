6ca837da837d11f67bbe9f458f41bac0f0640ef3
Devops Admin <devops@sastechstudio.com>
2022-03-15 19:55:06 +0000

    statushealth.sh edited online with Bitbucket

--- statushealth.sh
+++ statushealth.sh
@@ -1,20 +1,22 @@
   DEPLOYMENT_SUCCESS="false"
+  i=0
   #echo "Waiting for service deployment to complete..."
-  for((i=0;i<=10;i++))
-   do
-    status=$(aws ecs describe-services --cluster $2 --services $1 --region ap-south-1 && grep message && head -n 1 && grep steady)
-    # Wait to see if more than 1 deployment stays running
+ while [ $i -lt 10 ]
+  do
+    NUM_DEPLOYMENTS=$(aws ecs describe-services --services $1 --cluster $2 | jq "[.services[].deployments[]] | length")
+    if [ $NUM_DEPLOYMENTS -eq 1 ]; then
+  # Wait to see if more than 1 deployment stays running
     # If the wait time has passed, we need to roll back
-    if ( ${status} -eq 0); then
       echo "ecs service $1 running in $2"
       DEPLOYMENT_SUCCESS="true"
       break
       # Exit the loop.
-    else
+      else
       echo "Waiting for service $1 to be steady"
       sleep 30
+      i=$i+1
     fi
-   done
+  done
 
   if [[ "${DEPLOYMENT_SUCCESS}" != "true" ]]; then
     echo "ecs service $1 missing in $2"
