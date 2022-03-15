402ed94102495124d3c8718f88b2904284b42d16
Devops Admin <devops@sastechstudio.com>
2022-03-15 19:24:55 +0000

    statushealth.sh edited online with Bitbucket

--- statushealth.sh
+++ statushealth.sh
@@ -1,14 +1,25 @@
-#!/bin/bash
-echo "check ECS service exists"
-for((i=0;i<=10;i++))
-    do
-        status=$(aws ecs describe-services --cluster $2 --services $1 --query 'failures[0].reason' --output text)
-
-        if [[ "${status}" == "MISSING" ]]; then
-                echo "ecs service $1 missing in $2"
-                sleep 30
-        else
-                echo "ecs service $1 running in $2"
-                exit
+  DEPLOYMENT_SUCCESS="false"
+  every=5
+  i=0
+  TIMEOUT=60
+  echo "Waiting for service deployment to complete..."
+  while [ $i -lt $TIMEOUT ]
+  do
+          status=$(aws ecs describe-services --cluster $2 --services $1 --region ap-south-1 && grep message && head -n 1 && grep steady)
+    # Wait to see if more than 1 deployment stays running
+    # If the wait time has passed, we need to roll back
+    if ( ${status} -eq 0);then
+      echo "ecs service $1 running in $2"
+      DEPLOYMENT_SUCCESS="true"
+      # Exit the loop.
+      i=$TIMEOUT
+    else
+      sleep $every
+      i=$(( $i + $every ))
     fi
-    done
+  done
+
+  if [[ "${DEPLOYMENT_SUCCESS}" != "true" ]]; then
+    echo "ecs service $1 missing in $2"
+    fi
+    exit 1
