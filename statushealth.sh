5a888ad4f89b8e1ac432d0478ad2f4babc33bf11
RajeshPragnapuram <rajesh.pragnapuram@cloud4c.com>
2022-03-16 04:18:30 +0000

    statushealth.sh edited online with Bitbucket

--- statushealth.sh
+++ statushealth.sh
@@ -1,6 +1,6 @@
  #!/bin/bash
 
- for (( c=1; c<=5; c++ ))
+ for (( c=1; c<=10; c++ ))
 do
 aws ecs describe-services --cluster SASTech-Devops-Preprod --services uat-usermanagement --region ap-south-1 | grep message | head -n 1 | grep steady
 if [ $? -eq 0 ]
@@ -10,6 +10,6 @@ echo "server alive"
 break
 else
 echo "server dead or can not ping."
-sleep 10s
+sleep 45s
 fi
 done
