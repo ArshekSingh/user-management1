e310e4baa94eab3ee9f263817c021ffdeb6853a1
Devops Admin <devops@sastechstudio.com>
2022-03-16 08:12:25 +0000

    statushealth.sh edited online with Bitbucket

--- statushealth.sh
+++ statushealth.sh
@@ -6,12 +6,13 @@ aws ecs describe-services --cluster $2 --services $1 --region ap-south-1 | grep 
 if [ $? -eq 0 ]
 then
 echo "ecs service $1 is running on $2 "
+break
 else
 echo "Waiting for service $1 to be steady"
 sleep 45s
 $j = $(($j) + 1)
 fi
-if [ $j >= 1 ] 
+if [ $j == 10 ] 
 then 
 echo "Service $1 is not available in $2 Please check logs"
 fi
