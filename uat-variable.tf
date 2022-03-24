08ca9918463ced1b075ff3151518057ed77e6fee
d vinod kumar reddy <vinod.dugganavandla@cloud4c.com>
2022-03-24 09:10:32 +0000

    uat-variable.tf edited online with Bitbucket

--- uat-variable.tf
+++ uat-variable.tf
@@ -1,6 +1,6 @@
 variable "environment" {
     type= string
-    default= "prod"
+    default= "uat"
 }
 variable "aws_region" {
     type= string
@@ -16,11 +16,11 @@ variable "deployment_tag" {
 }
 variable "memory" {
     type= number
-    default=2048
+    default=1024
 }
 variable "cpu" {
     type= number
-    default= 1024
+    default= 256
 }
 variable "port" {
     type= number
@@ -40,7 +40,7 @@ variable "api_health"{
 }
 variable "vpc_id" {
     type= string
-    default="vpc-03f2eebc6fb8a5784"
+    default="vpc-0ef1c909227de196b"
 }
 variable "common_tags" {
     type= string
@@ -48,19 +48,19 @@ variable "common_tags" {
 }
 variable "containers_min" {
     type= number
-    default=2
+    default=1
 }
 variable "containers_max" {
     type= number
-    default=4
+    default=1
 }
 variable "clustername" {
     type= string
-    default="prod-sas-ecs"
+    default="SASTech-Devops-Preprod"
 }
 variable "lb_listener_arn"{
     type = string
-    default="arn:aws:elasticloadbalancing:ap-south-1:305949049023:listener/app/sastech-svcl-prod-alb/39b5a698ac5d9cdf/57b90b675e845a07"
+    default="arn:aws:elasticloadbalancing:ap-south-1:305949049023:listener/app/Devops-ALB/e1dd7790c5b34474/2b8b3ad1ea006326"
 }
 variable "service_role"{
     type= string
