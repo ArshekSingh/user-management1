684c7f07f9ccf449ede407209812e7b414a88e9b
Ranjith Velamala <ranjith.velamala@cloud4c.com>
2022-03-25 08:13:56 +0000

    statushealth.sh

--- statushealth.sh
+++ statushealth.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-$j=0
 for (( i=1; i<=10; i++ ))
 do
 aws ecs describe-services --cluster $2 --services $1 --region ap-south-1 | grep message | head -n 1 | grep steady
@@ -10,11 +9,11 @@ break
 else
 echo "Waiting for service $1 to be steady"
 sleep 45s
-$j = $(($j) + 1)
 fi
-if [ $j == 10 ] 
-then 
+if [ $i == 10 ]
+then
 echo "Service $1 is not available in $2 Please check logs"
+exit 0
 fi
 done
 
