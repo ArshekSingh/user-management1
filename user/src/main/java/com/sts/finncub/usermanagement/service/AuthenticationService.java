74d12d05a7c3907700b2cb8d8db54f0fd18b58cf
Saloni Bansal <saloni.bansal@sastechstudio.com>
2022-06-07 15:13:34 +0530

    FLM-823 : Add downtime for application

--- user/src/main/java/com/sts/finncub/usermanagement/service/AuthenticationService.java
+++ user/src/main/java/com/sts/finncub/usermanagement/service/AuthenticationService.java
@@ -1,9 +1,5 @@
 package com.sts.finncub.usermanagement.service;
 
-import javax.servlet.http.HttpServletRequest;
-
-import org.springframework.http.ResponseEntity;
-
 import com.sts.finncub.core.exception.BadRequestException;
 import com.sts.finncub.core.exception.InternalServerErrorException;
 import com.sts.finncub.core.exception.ObjectNotFoundException;
@@ -12,13 +8,16 @@ import com.sts.finncub.usermanagement.request.LoginRequest;
 import com.sts.finncub.usermanagement.request.SignupRequest;
 import com.sts.finncub.usermanagement.response.LoginResponse;
 import com.sts.finncub.usermanagement.response.SignupResponse;
+import org.springframework.http.ResponseEntity;
+
+import javax.servlet.http.HttpServletRequest;
 
 public interface AuthenticationService {
     LoginResponse login(LoginRequest loginRequest) throws ObjectNotFoundException, BadRequestException, InternalServerErrorException;
 
     SignupResponse signup(SignupRequest signupRequest) throws BadRequestException;
 
-	Response verify(String authToken) throws ObjectNotFoundException;
+    Response verify(String authToken) throws ObjectNotFoundException;
 
     ResponseEntity<Response> logout(HttpServletRequest request);
 
