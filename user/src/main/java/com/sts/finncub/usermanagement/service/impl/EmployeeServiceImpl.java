9efb81d80eb78529bcf6eaa9b763c9e9d9bc1db1
Shahzad Hussain <shahzad.hussain@sastechstudio.com>
2022-07-14 17:36:20 +0530

    remove duplicate call for total count

--- user/src/main/java/com/sts/finncub/usermanagement/service/impl/EmployeeServiceImpl.java
+++ user/src/main/java/com/sts/finncub/usermanagement/service/impl/EmployeeServiceImpl.java
@@ -29,7 +29,6 @@ import com.sts.finncub.usermanagement.service.UserService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
