42d9507ddc2d75538e5eee026be714a86de1955e
arshek-singh <arshek.singh@parallelcap.in>
2022-11-22 14:38:30 +0530

    FLM-1522 Updated message for send sms.

--- user/src/main/java/com/sts/finncub/usermanagement/service/impl/AuthenticationServiceImpl.java
+++ user/src/main/java/com/sts/finncub/usermanagement/service/impl/AuthenticationServiceImpl.java
@@ -31,6 +31,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import java.math.BigDecimal;
