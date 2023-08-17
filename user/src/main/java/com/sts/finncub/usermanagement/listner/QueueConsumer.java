ea88d875ae1a898adb9eaf56344b4eecbbaf500f
singharshek <120161107+singharshek@users.noreply.github.com>
2023-08-17 16:50:55 +0530

    FLM-2346 - created new queue for uat.

--- user/src/main/java/com/sts/finncub/usermanagement/listner/QueueConsumer.java
+++ user/src/main/java/com/sts/finncub/usermanagement/listner/QueueConsumer.java
@@ -6,6 +6,7 @@ import com.sts.finncub.core.dto.SmsQueueRequest;
 import com.sts.finncub.usermanagement.service.SmsService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener;
 import org.springframework.stereotype.Component;
 
@@ -16,7 +17,10 @@ public class QueueConsumer {
 
     private final SmsService smsService;
 
-    @SqsListener(value = "finncub-sms-queue")
+    @Value("${queue.name}")
+    private String queueName;
+
+    @SqsListener(value = queueName)
     public void loadMessageFromSQS(String message) throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
         SmsQueueRequest smsQueueRequest = mapper.readValue(message, SmsQueueRequest.class);
